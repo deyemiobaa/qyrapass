76e237a269294ee0e0fa449e782f5d5b62fc9418
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2025-04-03 14:51:44 +0000

    remove test workflow

--- test/controllers/qr_codes_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class QrCodesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get qr_codes_index_url
-    assert_response :success
-  end
-end
